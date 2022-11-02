76100d67b1a50dafb97aaa2c6df5dd58bff10dc7
Alan Cashman <nocabbage@gmail.com>
2022-11-01 19:24:17 -0700

    finished exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+	const tempC = (temp - 32) * (5 / 9);
+	if (tempC === 0) return tempC;
+	else return +tempC.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+	const tempF = temp * (9 / 5) + 32;
+	if (tempF === 0) return tempF;
+	else return +tempF.toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+	ftoc,
+	ctof,
 };
