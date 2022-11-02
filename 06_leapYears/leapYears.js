1f170ad14a459ce5de4e6b70c997959ac97bfd41
Alan Cashman <nocabbage@gmail.com>
2022-11-01 19:14:55 -0700

    finished exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+	if (year % 4 == 0 && year % 100 != 0) return true;
+	else if (year % 400 == 0) return true;
+	else return false;
 };
+
+// 1. if year is divisble by 4, it's a leap year - year % 4 = 0
+// 2. EXCEPT when the year is divisible by 100 - year % 100 != 0
+// 3. UNLESS that year is also divisible by 400 - year % 400 = 0
 
 // Do not edit below this line
 module.exports = leapYears;
